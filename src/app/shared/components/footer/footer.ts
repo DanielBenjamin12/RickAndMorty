a95636cc6e01cf11825bf948ab158da02b751161
DanielBenjamin12 <benjamindaniel711@gmail.com>
2026-02-08 18:52:28 -0400

    feat: Implement initial application layout with Header and Footer components
    This commit introduces the foundational layout for the application, including:
    
    - Creation and integration of Header and Footer Angular components.
    - Setup of Angular Material, including @angular/cdk and @angular/material dependencies.
    - Definition of a custom Material theme via material-theme.scss.
    - Integration of Google Fonts (Roboto) and Material Icons.
    - Introduction of a new application logo (logo.png) and an updated favicon.
    - Establishment of a basic page structure in pp.html utilizing the new components.

---
+++ src/app/shared/components/footer/footer.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-footer',
+  imports: [MatDividerModule, MatIconModule],
+  templateUrl: './footer.html',
+  styles: '',
+})
+export class Footer {
+
+}
